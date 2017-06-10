0dcd551858cc5240994ce6212873ff3cd68cb428
serega <serega47_92@mail.ru>
2017-06-10 23:54:40 +0700

    Была переделана структура проекта. Выделены отдельно файлы под соответствующие задачи.

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,5 @@
  * Custom
  */
 //= partials/app.js
+//= partials/block-1-carousel.js
+//= partials/calculator.js
---
+++ src/js/partials/block-1-carousel.js
@@ -0,0 +1,106 @@
+"use strict"
+
+window.onload = function(){
+
+  //находим слайдер на сцене и создаём объект слайдер
+  var slider = new Slider(document.getElementById("slider"));
+
+  setInterval(slider.sliderNextElement, 8000);
+}
+
+function Slider(slider){
+  //находим элементы слайдера и превращаем их из коллекции в массив
+  var sliderElements = [].slice.call(getSliderElements());
+
+  var classPositionLeft = "slide__position__left";
+  var classPositionCenter = "slide__position__center";
+  var classPositionRight = "slide__position__right";
+
+  var classHide = "slide__hide";
+  var classShow = "slide__show";
+
+  var isAnimatedNow = false;
+
+  // var classSliderBackgrounds = ["light_background","dark_background"];
+  var classMarkersBackgrounds = ["slider__markers__position-left","slider__markers__position-right"];
+
+  //показывает реальную очерёдность слайдов в стеке
+  var numberList = [0,1];
+
+  //маркеры
+  var markers = document.getElementById("slider__markers__position");
+  markers.addEventListener("click", sliderNextElement);
+
+
+  function getSliderElements(){
+    return slider.getElementsByClassName("block__1__slide__element");
+  }
+
+  this.sliderNextElement = sliderNextElement;
+  function sliderNextElement(){
+
+    if(isAnimatedNow) return;
+
+    sliderElements[0].classList.remove(classPositionCenter);
+    sliderElements[0].classList.add(classPositionLeft);
+
+    sliderElements[1].classList.remove(classPositionLeft);
+    sliderElements[1].classList.add(classPositionCenter);
+
+    isAnimatedNow = true;
+
+    PrefixedEvent(sliderElements[1], "animationend", function(){
+
+      isAnimatedNow = false;
+
+    })
+
+    //changeBackground();
+
+    shiftElements();
+    changeMarkers();
+  }
+
+  function shiftElements(){
+    console.log(sliderElements);
+
+    sliderElements.push(sliderElements.shift());
+
+    console.log(sliderElements);
+  }
+
+  function changeMarkers(){
+    
+    if(markers.classList.contains(classMarkersBackgrounds[0])){
+      markers.classList.remove(classMarkersBackgrounds[0]);
+      markers.classList.add(classMarkersBackgrounds[1]);
+    } else {
+      markers.classList.remove(classMarkersBackgrounds[1]);
+      markers.classList.add(classMarkersBackgrounds[0]);
+    }
+  }
+
+  function changeBackground(){
+    if(slider.classList.contains(classSliderBackgrounds[0])){
+      slider.classList.remove(classSliderBackgrounds[0]);
+      slider.classList.add(classSliderBackgrounds[1]);
+    } else {
+      slider.classList.remove(classSliderBackgrounds[1]);
+      slider.classList.add(classSliderBackgrounds[0]);
+    }
+  }
+
+  // this.showSliderElements = function(){
+  //   console.log(sliderElements);
+  // }
+}
+
+//'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd'
+
+var pfx = ["webkit", "moz", "MS", "o", ""];
+function PrefixedEvent(element, type, callback) {
+    for (var p = 0; p < pfx.length; p++) {
+        if (!pfx[p]) type = type.toLowerCase();
+        element.addEventListener(pfx[p] + type, callback, false);
+    }
+}
