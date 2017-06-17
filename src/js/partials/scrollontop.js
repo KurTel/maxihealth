6cc3192a72748c3cd351ae7b4c55f2f4127f186e
serega <serega47_92@mail.ru>
2017-06-17 12:47:10 +0700

    ScrollOnTop переведён обратно на свой код, со сторонним кодом лаг, доходя до верха страницы лагает прокрутка вниз.

--- src/js/partials/scrollontop.js
+++ src/js/partials/scrollontop.js
@@ -1,20 +1,20 @@
 "use strict"
 
-// function scrollOnTop(scrollDuration) {
-// const   scrollHeight = window.scrollY,
-//         scrollStep = Math.PI / ( scrollDuration / 15 ),
-//         cosParameter = scrollHeight / 2;
-// var     scrollCount = 0,
-//         scrollMargin,
-//         scrollInterval = setInterval( function() {
-//             if ( window.scrollY != 0 ) {
-//                 scrollCount = scrollCount + 1;  
-//                 scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
-//                 window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
-//             } 
-//             else clearInterval(scrollInterval); 
-//         }, 15);
-// }
+function scrollOnTop(scrollDuration) {
+const   scrollHeight = window.scrollY,
+        scrollStep = Math.PI / ( scrollDuration / 15 ),
+        cosParameter = scrollHeight / 2;
+var     scrollCount = 0,
+        scrollMargin,
+        scrollInterval = setInterval( function() {
+            if ( window.scrollY != 0 ) {
+                scrollCount = scrollCount + 1;  
+                scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+                window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+            } 
+            else clearInterval(scrollInterval); 
+        }, 15);
+}
 
 onloadInit.push( function(){
   
